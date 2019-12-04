72960d2325efdf934041271ae666a82199913af6
Chelsea Sidrane <csidrane@stanford.edu>
2019-12-03 17:40:32 -0800

    nominally working

--- src/theory/idl/theory_idl.cpp
+++ src/theory/idl/theory_idl.cpp
@@ -78,38 +78,7 @@ Node TheoryIdl::ppRewrite(TNode atom) {
       << "TheoryIdl::ppRewrite(): processing " << atom << std::endl;
   NodeManager* nm = NodeManager::currentNM();
 
-  // // handle the case of (op (x y))
-  // if ((atom[0].getKind() != kind::MINUS) && (atom[1].getKind() != kind::MINUS) 
-  //           && (atom.getKind() != kind::AND) && (atom.getKind() != kind::OR)) 
-  //   {
-  //     Node left = nm->mkNode(kind::MINUS, atom[0], atom[1]);
-  //     Node right = nm->mkConst(Rational(0));
-  //     atom = nm->mkTNode(atom.getKind(), left, right);
-  //   }
-
-  // // handle the case of (op n (- x y)) [where we want (op (- x y) n)] (LT, LTE)
-  // if((atom[1].getKind() == kind::MINUS) && ((atom.getKind() == kind::LT) || 
-  //                                           (atom.getKind() == kind::LEQ) || 
-  //                                           (atom.getKind() == kind::EQUAL) 
-  //                                          )
-  //   )
-  //   {
-  //     Node negated_right = nm->mkNode(kind::MINUS, atom[1][1], atom[1][0]);
-  //     const Rational& left = atom[0].getConst<Rational>();
-  //     Node negated_left = nm->mkConst(-left);
-  //     atom = nm->mkNode(atom.getKind(), negated_right, negated_left);
-  //   }
-  // // handle the case of (op (- x y) n) where we want (op n (- x y)) (GT, GTE)
-  // else if((atom[0].getKind() == kind::MINUS) && ((atom.getKind() == kind::GT) || (atom.getKind() == kind::GEQ)))
-  //   {
-  //     Node negated_left = nm->mkNode(kind::MINUS, atom[0][1], atom[0][0]);
-  //     const Rational& right = atom[1].getConst<Rational>();
-  //     Node negated_right = nm->mkConst(-right);
-  //     atom = nm->mkNode(atom.getKind(), negated_right, negated_left);
-  //   }
-   
-  // printf(atom)
-
+  // assume at most 1 unary minus (no nested unary minuses)
   switch (atom.getKind())
   {
     case kind::EQUAL:
@@ -127,8 +96,16 @@ Node TheoryIdl::ppRewrite(TNode atom) {
     {
       if(atom[0].getKind() == kind::MINUS)
       {
-        Node right_minus_1 = nm->mkConst(atom[1].getConst<Rational>() - Rational(1));
-        return nm->mkNode(kind::LEQ, atom[0], right_minus_1);
+        // if(atom[1].getKind() == kind::UMINUS)
+        // {
+        //   const Rational& neg_n = atom[1][0].getConst<Rational>()
+        //   Node n_minus_1 = -neg_n - Rational(1));
+        // }
+        // else // assume constant
+        // {
+          Node n_minus_1 = nm->mkConst(atom[1].getConst<Rational>() - Rational(1));
+        // }
+        return nm->mkNode(kind::LEQ, atom[0], n_minus_1);
       }
       else if (atom[1].getKind() == kind::MINUS)
       {
@@ -266,14 +243,18 @@ void TheoryIdl::check(Effort level) {
 bool TheoryIdl::collectModelInfo(TheoryModel* m)
 {
   std::vector<Rational> distance(d_numVars, Rational(0));
-
+  //  cout << d_numVars << endl;
+  //  cout << distance[0] << endl;
+  //  cout << distance[1] << endl;
+  //  cout << distance[2] << endl;
+  //  cout << distance[3] << endl;
   // ---------------------------------------------------------------------------
   // TODO: implement model generation by computing the single-source shortest
   // path from a node that has distance zero to all other nodes
   // ---------------------------------------------------------------------------
-    // Ford-Bellman, "relax" edges |V| - 1 times
-    // Should |V| count the new vertex, s, or not?
-  for (int i = 0; i <= d_numVars - 2; ++i)
+    // Ford-Bellman, "relax" edges at least |V| - 1 times
+    // (no cycles so it shouldn't matter if we do it more than |V| - 1 times!)
+  for (int i = 0; i <= d_numVars; ++i)
   {
     for (int v1 = 0; v1 <= d_numVars - 1; ++v1)
     {
@@ -291,6 +272,11 @@ bool TheoryIdl::collectModelInfo(TheoryModel* m)
     }
   }
 
+  // negate distances as per piazza post #30
+  for (size_t i = 0; i < d_numVars; i++)
+  {
+    distance[i] = -distance[i];
+  }
 
   NodeManager* nm = NodeManager::currentNM();
   for (size_t i = 0; i < d_numVars; i++)
